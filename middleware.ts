bb32679a8b60e6878366d2c31c29c0ea4fdb4add
mrvin100 <vincentyoumssi@example.com>
2025-01-20 09:14:25 +0100

    add: remove verification for captcha

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest){
-    const isCaptchaVerified = request.cookies.get("captcha_verified")
+    // const isCaptchaVerified = request.cookies.get("captcha_verified")
 
-    if(!isCaptchaVerified && request.nextUrl.pathname !== "/captcha"){
-        return NextResponse.redirect(new URL("/captcha", request.url))
-    }
+    // if(!isCaptchaVerified && request.nextUrl.pathname !== "/captcha"){
+    //     return NextResponse.redirect(new URL("/captcha", request.url))
+    // }
+    const isCaptchaVerified = true
+
+    // if(!isCaptchaVerified && request.nextUrl.pathname !== "/captcha"){
+    //     return NextResponse.redirect(new URL("/captcha", request.url))
+    // }
 
     return NextResponse.next()
 }
